afe3fb8a075d90455898912d474c110702471b5d
karoui-ma <112547056+karoui-ma@users.noreply.github.com>
2023-11-14 00:50:24 +0100

    Revert "Karoui"

--- app/src/main/java/com/appsnipp/education/ui/Events/ListEvent.java
+++ app/src/main/java/com/appsnipp/education/ui/Events/ListEvent.java
@@ -2,7 +2,9 @@
  * Copyright (c) 2023. rogergcc
  */
 
-package com.appsnipp.education.ui.Courses;
+package com.appsnipp.education.ui.Events;
+
+import static android.os.Build.VERSION_CODES.R;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,37 +14,34 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.appsnipp.education.R;
-import com.appsnipp.education.ui.Events.AddEvent;
-import com.appsnipp.education.ui.Events.ListEvent;
-import com.appsnipp.education.ui.Events.UpdateEvent;
 
-public class ListCourses extends AppCompatActivity {
+public class ListEvent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.listcourses);
+        setContentView(com.appsnipp.education.R.layout.listevent);
 
         // Obtenir une référence au bouton "modifier" par son ID
         Button modifierButton = findViewById(com.appsnipp.education.R.id.modifier);
-        Button ajouterButton = findViewById(com.appsnipp.education.R.id.add);
+        Button ajouterButton = findViewById(com.appsnipp.education.R.id.button);
 
         // Ajouter un écouteur de clic au bouton
         modifierButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Rediriger vers "eventupdate.xml" en créant une nouvelle intention (Intent)
-                Intent intent = new Intent(ListCourses.this, UpdateCourses.class);
+                Intent intent = new Intent(ListEvent.this, UpdateEvent.class);
                 startActivity(intent);
             }
         });
-        ajouterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Rediriger vers "eventupdate.xml" en créant une nouvelle intention (Intent)
-                Intent intent = new Intent(ListCourses.this, AddCourses.class);
-                startActivity(intent);
-            }
-        });
+                ajouterButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Rediriger vers "eventupdate.xml" en créant une nouvelle intention (Intent)
+                        Intent intent = new Intent(ListEvent.this, AddEvent.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
